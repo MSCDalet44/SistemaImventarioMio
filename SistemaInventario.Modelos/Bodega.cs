2053f4525355c1c6b9227f0262b5bcdafce2ac91
MSCDalet44 <isc.villanueva@hotmail.com>
2024-02-14 23:18:57 -0600

    Configuración del Proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El Campo Nombre es REQUERIDO")]
+        [MaxLength(60,ErrorMessage ="El Nombre no debe de exceder de 60 Caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El Campo Descripción es REQUERIDO")]
+        [MaxLength(100, ErrorMessage = "La Descripción no debe de exceder de 100 Caracteres")]
+        public string Descripcion  { get; set; }
+        [Required]
+        public bool Estado { get; set; }
+    }
+}
